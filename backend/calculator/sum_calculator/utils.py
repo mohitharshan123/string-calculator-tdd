0779b6a4091bd67b31e94009437dd17567de84cb
Mohit <mohit@Mohits-MacBook-Pro-3.local>
2024-08-30 00:19:18 +0530

    updated util to validate for negative number and raise exception

--- backend/calculator/sum_calculator/utils.py
+++ backend/calculator/sum_calculator/utils.py
@@ -1,4 +1,5 @@
 import re
+from .exceptions import NegativeNumberException
 
 class Calculator:
     def __init__(self, numbers):
@@ -7,6 +8,8 @@ class Calculator:
 
     def add(self):                
         numbers_array = self._get_numbers_array()
+        self._validate_numbers(numbers_array)
+        
         return sum(n for n in numbers_array if n <= 1000)
     
     def _get_numbers_array(self):
@@ -14,3 +17,9 @@ class Calculator:
         numbers_array = numbers_string.split(",")
                 
         return [int(n) for n in numbers_array if n]
+    
+    
+    def _validate_numbers(self, numbers):
+        negatives = [n for n in numbers if n < 0]
+        if negatives:
+            raise NegativeNumberException(f"negatives not allowed: {', '.join(map(str, negatives))}")
