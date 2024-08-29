8944670e6039cf935c551ef28094b2933feeef50
Mohit <mohit@Mohits-MacBook-Pro-3.local>
2024-08-30 00:15:22 +0530

    Updated util to handle num > 1000

--- backend/calculator/sum_calculator/utils.py
+++ backend/calculator/sum_calculator/utils.py
@@ -1,12 +1,16 @@
-
+import re
 
 class Calculator:
     def __init__(self, numbers):
         self.numbers = numbers
         self.delimiters = [',', '\n']
 
-    def add(self):
-        numbers_array = self.numbers.split(",") 
-                
-        return sum(int(n) for n in numbers_array if n)
+    def add(self):                
+        numbers_array = self._get_numbers_array()
+        return sum(n for n in numbers_array if n <= 1000)
     
+    def _get_numbers_array(self):
+        numbers_string = re.sub(r'[\r\n]+', ',', self.numbers)
+        numbers_array = numbers_string.split(",")
+                
+        return [int(n) for n in numbers_array if n]
